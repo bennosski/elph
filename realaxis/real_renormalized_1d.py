c018bda62431431c9b7aab28353c5e8b7fe9ff4e
bennosski <bennosski@gmail.com>
2019-08-21 11:16:32 -0400

    update

--- realaxis/real_renormalized_1d.py
+++ realaxis/real_renormalized_1d.py
@@ -1,5 +1,5 @@
 import imagaxis
-from renormalized_2d import Migdal
+from renormalized_1d import Migdal
 from convolution import conv
 from functions import band_1dsquare_lattice, mylamb2g0
 import os
