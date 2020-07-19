updated selfconsistency with tail corrected filling calculation

imagaxis/base.py
@@ -9,7 +9,6 @@ from scipy.ndimage.filters import gaussian_filter
 from functions import lamb2g0_ilya
 import fourier
 import matplotlib 
-matplotlib.use('TkAgg')
 from matplotlib.pyplot import *
 from anderson import AndersonMixing
 
@@ -77,6 +76,9 @@ class MigdalBase:
 
     def compute_n(self, G): pass
 
+   
+    def compute_n_tail(self, wn, ek, mu): pass
+
 
     def compute_G(self, wn, ek, mu, S): pass
 
@@ -93,6 +95,9 @@ class MigdalBase:
     def init_selfenergies(self): pass
 
 
+    def n_from_Sw(self, wn, ek, mu, Sw): pass
+
+
     def dyson_fermion(self, wn, ek, mu, S, axis):
         Sw, jumpS = fourier.t2w(S, self.beta, axis, 'fermion')
 
@@ -100,11 +105,34 @@ class MigdalBase:
             mu = self.fixed_mu
         else:
             if abs(self.dens-1.0)>1e-10:
+
+                mu_new = optimize.fsolve(lambda mu : self.compute_fill(self.compute_G(wn,ek,mu,Sw)) + self.compute_n_tail(wn, ek, mu) - self.dens, mu)[0]
+                mu = mu_new*0.9 + mu*0.1
+            else:
+                mu = 0
+
+        
+        # old way : misses the contribution to n from the sum in the tails
+        '''
+        else:
+            if abs(self.dens-1.0)>1e-10:
                 mu_new = optimize.fsolve(lambda mu : self.compute_fill(self.compute_G(wn,ek,mu,Sw))-self.dens, mu)[0]
                 mu = mu_new*0.9 + mu*0.1
             else:
                 mu = 0
-            
+        '''
+
+
+        '''
+        # this is for density based on sum over iwn. 
+        # But this is not jump corrected (missing the infinite tail so, probably it is less accurate than using the tau value)
+        else:
+            if abs(self.dens-1.0)>1e-10:
+                mu_new = optimize.fsolve(lambda mu : self.compute_fill(self.compute_G(wn,ek,mu,Sw))-self.dens, mu)[0]
+                mu = mu_new*0.9 + mu*0.1
+            else:
+                mu = 0
+        '''
 
         Gw = self.compute_G(wn, ek, mu, Sw)        
 
@@ -170,6 +198,23 @@ class MigdalBase:
 
         print('\nImag-axis selfconsistency\n--------------------------')
 
+        '''
+        # understand filling differences
+        mu, G = self.dyson_fermion(wn, ek, mu, S0, self.dim)
+        n_tau = self.compute_n(G)
+        Gw, jump = fourier.t2w(G, self.beta, 2, 'fermion')
+        print('Gw shape', Gw.shape)
+        n_w = self.compute_fill(Gw)
+        print('n_tau', n_tau)
+        print('n_w', n_w)
+        print('expected missing part')
+        mp = self.compute_n_tail(wn, ek, mu)
+        print('mp', mp)
+        print('correct fill', n_w + mp)
+        exit()
+        '''
+        
+
         if S0 is None: 
             S, PI = self.init_selfenergies()
         else:
@@ -191,7 +236,7 @@ class MigdalBase:
 
             '''
             if abs(self.dens-1.0)>1e-10:
-                mu -= 0.01*(n-self.dens)/dndmu
+                mu -= 0.1*(n-self.dens)/dndmu
             '''
 
             # compute new selfenergies S(tau) and PI(tau)

imagaxis/migdal_2d.py
@@ -6,14 +6,24 @@ import os
 import time
 import matplotlib.pyplot as plt
 import numpy as np
+import fourier
+
 
 class Migdal(MigdalBase):
     #------------------------------------------------------------
     def compute_fill(self, Gw):
-        return real(1.0 + 2./(self.nk**2 * self.beta) * 2.0*sum(Gw))
+        return real(1.0 + 2./(self.nk**2 * self.beta) * 2.0*np.sum(Gw))
     #------------------------------------------------------------
     def compute_n(self, Gtau):
         return -2.0*mean(Gtau[:,:,-1]).real
+
+
+    def compute_n_tail(self, wn, ek, mu):
+        baresum = 1 + 4 * np.mean(1/4 * np.tanh(-self.beta*(ek-mu)/2))
+        bareGw = self.compute_G(wn, ek, mu, 0)
+        return baresum - (1 + 2/(self.nk**2 * self.beta) * 2*np.sum(bareGw.real))
+
+
     #------------------------------------------------------------
     def compute_G(self, wn, ek, mu, S):
         return 1.0/(1j*wn[None,None,:] - (ek[:,:,None]-mu) - S)
@@ -57,6 +67,27 @@ class Migdal(MigdalBase):
         else:
             return 1 - self.g0**2/self.nk**2 * conv(F0gamma, D, ['k-q,q','k-q,q', 'm,n-m'], [0,1,2], [True,True,False], self.beta, kinds=('fermion','boson','fermion'), op='...,...', jumps=(jumpF0gamma, jumpD))
 
+
+    def compute_jjcorr(self, G): 
+        ''' 
+        input is G(tau)
+        returns jjcorr(q=0, tau)
+        '''
+        ks = np.arange(-np.pi, np.pi, 2*np.pi/self.nk)
+        sin = np.sin(ks)
+        cos = np.cos(ks)
+        fk = (-2*self.t*sin[:,None] - 4*self.tp*sin[:,None]*cos[None,:])
+        jj0t = 2.0 / self.nk**2 * np.sum((fk**2)[:,:,None] * G * G[:,:,::-1], axis=(0,1))
+
+        np.save('jj0t{}'.format('r' if self.renormalized else 'u'), jj0t)
+         
+        jj0v = fourier.t2w(jj0t, self.beta, 0, 'boson')        
+        jjv = jj0v / (1 + self.g0**2 * (-2/self.omega) * jj0v)
+
+        ivn = 2*np.arange(len(jjv))*np.pi/self.beta
+
+        np.save('jjv{}'.format('r' if self.renormalized else 'u'), jjv)
+    
 
 
 if __name__=='__main__':

realaxis/a2F.py
@@ -410,11 +410,13 @@ def corrected_a2F(basedir, folder, ntheta=5):
     # get B interp
     B = -1.0/np.pi * DR.imag
     
+    max_w = 0.3
+    max_iw = np.argmin(np.abs(wr - max_w))
     
     a2F = np.zeros(nr)
     lambk = np.zeros((ntheta, nr))
     
-    for iw in range(nr//2, nr):
+    for iw in range(nr//2, max_iw):
         print(iw, end=' ')
         ks = np.linspace(-np.pi, np.pi, nk+1)
         I = interp2d(ks, ks, B[:,:,iw], kind='linear')

realaxis/real_2d.py
@@ -8,6 +8,7 @@ import fourier
 from anderson import AndersonMixing
 import matplotlib.pyplot as plt
 
+
 class RealAxisMigdal(Migdal):
     #-----------------------------------------------------------
     def __init__(self, params, basedir):
@@ -30,7 +31,8 @@ class RealAxisMigdal(Migdal):
         wn = (2*np.arange(self.nw)+1) * np.pi / self.beta
         vn = (2*np.arange(self.nw+1)) * np.pi / self.beta
         ek = self.band(self.nk, self.t, self.tp)
-        
+        #ek = band_square_lattice(self.nk, self.t, self.tp)        
+
         return wn, vn, ek, w, nB, nF, DRbareinv
     #----------------------------------------------------------- 
     def compute_GR(self, w, ek, mu, SR):
@@ -67,6 +69,46 @@ class RealAxisMigdal(Migdal):
             return 2.0*self.g0**2*self.dw/self.nk**2*(basic_conv(A, Gsum, ['k+q,k','k+q,k','z,w-z'], [0,1,2], [True,True,False])[:,:,:self.nr] \
                -basic_conv(A, GA*nF[None,None,:], ['k+q,k','k+q,k','w+z,z'], [0,1,2], [True,True,False])[:,:,:self.nr] \
                +basic_conv(A*nF[None,None,:], GA, ['k+q,k','k+q,k','w+z,z'], [0,1,2], [True,True,False])[:,:,:self.nr])
+
+
+    def compute_jjcorr(self, w, wn, G, GR, nF):
+        # convert to imaginary frequency
+        G = fourier.t2w(G, self.beta, self.dim, 'fermion')[0]
+
+        # compute Gsum
+        Gsum  = np.zeros([self.nk,self.nk,self.nr], dtype=complex)
+        for i in range(self.nr):
+            Gsum[:,:,i]  = np.sum(G/((w[i]+1j*wn)[None,None,:]), axis=2) / self.beta 
+        # handle sum over pos and negative freqs
+        Gsum  += np.conj(Gsum)
+        print('finished Gsum')    
+
+        del G
+
+        ks = np.arange(-np.pi, np.pi, 2*np.pi/self.nk)
+        sin = np.sin(ks)
+        cos = np.cos(ks)
+        fk = (-2*self.t*sin[:,None] - 4*self.tp*sin[:,None]*cos[None,:])
+        GA = np.conj(GR)
+        A  = -1.0/np.pi * GR.imag
+
+        A *= (fk**2)[:,:,None]
+        jj0w = 2.0 * self.dw / self.nk**2 * np.sum(
+                basic_conv(A, Gsum, ['z,w-z'], [2], [False])[:,:,:self.nr] \
+               -basic_conv(A, GA*nF[None,None,:], ['w+z,z'], [2], [False])[:,:,:self.nr] \
+               +basic_conv(A*nF[None,None,:], GA, ['w+z,z'], [2], [False])[:,:,:self.nr], axis=(0,1))
+
+        np.save('jj0w{}'.format('r' if self.renormalized else 'u'), jj0w)
+
+        del GA
+        del A
+        del Gsum
+
+        print('done jj0w')
+
+        jjw = jj0w / (1 + self.g0**2 * (-2/self.omega) * jj0w)
+        np.save('jjw{}'.format('r' if self.renormalized else 'u'), jjw)
+
     
     #-----------------------------------------------------------    
     def selfconsistency(self, sc_iter, frac=0.5, alpha=0.5, S0=None, PI0=None, mu0=None, cont=False):