fixed current-current correlator calculation (frequency dependence)

imagaxis/migdal_2d.py
@@ -68,7 +68,7 @@ class Migdal(MigdalBase):
             return 1 - self.g0**2/self.nk**2 * conv(F0gamma, D, ['k-q,q','k-q,q', 'm,n-m'], [0,1,2], [True,True,False], self.beta, kinds=('fermion','boson','fermion'), op='...,...', jumps=(jumpF0gamma, jumpD))
 
 
-    def compute_jjcorr(self, G): 
+    def compute_jjcorr(self, G, D): 
         ''' 
         input is G(tau)
         returns jjcorr(q=0, tau)
@@ -80,9 +80,11 @@ class Migdal(MigdalBase):
         jj0t = 2.0 / self.nk**2 * np.sum((fk**2)[:,:,None] * G * G[:,:,::-1], axis=(0,1))
 
         np.save('jj0t{}'.format('r' if self.renormalized else 'u'), jj0t)
+
+        Dw = fourier.t2w(D, self.beta, 2, 'boson')
          
         jj0v = fourier.t2w(jj0t, self.beta, 0, 'boson')        
-        jjv = jj0v / (1 + self.g0**2 * (-2/self.omega) * jj0v)
+        jjv = jj0v / (1 + self.g0**2 * Dw[self.nk//2, self.nk//2] * jj0v)
 
         ivn = 2*np.arange(len(jjv))*np.pi/self.beta
 

realaxis/real_2d.py
@@ -58,7 +58,7 @@ class RealAxisMigdal(Migdal):
                +basic_conv(B, GR*nF[None,None,:], ['q,k-q','q,k-q','z,w-z'], [0,1,2], [True,True,False])[:,:,:self.nr])
 
     #-----------------------------------------------------------
-    def compute_PIR(self, GR, Gsum, nF):
+    def compute_PIR(self, GR, Gsum, nF, DR):
         GA = np.conj(GR)
         A  = -1.0/np.pi * GR.imag
         if hasattr(self, 'gq2'):
@@ -71,7 +71,7 @@ class RealAxisMigdal(Migdal):
                +basic_conv(A*nF[None,None,:], GA, ['k+q,k','k+q,k','w+z,z'], [0,1,2], [True,True,False])[:,:,:self.nr])
 
 
-    def compute_jjcorr(self, w, wn, G, GR, nF):
+    def compute_jjcorr(self, w, wn, G, GR, nF, DR):
         # convert to imaginary frequency
         G = fourier.t2w(G, self.beta, self.dim, 'fermion')[0]
 
@@ -106,7 +106,7 @@ class RealAxisMigdal(Migdal):
 
         print('done jj0w')
 
-        jjw = jj0w / (1 + self.g0**2 * (-2/self.omega) * jj0w)
+        jjw = jj0w / (1 + self.g0**2 * DR[self.nk//2, self.nk//2] * jj0w)
         np.save('jjw{}'.format('r' if self.renormalized else 'u'), jjw)
 
     