60e882c9c49058e3cfe550a73336d98b7f6fb3ca
Ben <bennosski@gmail.com>
2020-07-30 10:45:18 -0700

    Added shifted zero-index option to convolution

--- imagaxis/convolution.py
+++ imagaxis/convolution.py
@@ -2,9 +2,8 @@ from numpy import *
 import fourier
 
 
-def basic_conv(a, b, indices_list, axes, circular_list, op='...,...'):
+def basic_conv(a, b, indices_list, axes, circular_list, op='...,...', izero=None):
     '''
-    
     todo: write optional zeros argument
       - if x is the repeated index, then yz-xz=0 and xz=0 and yz=0
     
@@ -24,9 +23,78 @@ def basic_conv(a, b, indices_list, axes, circular_list, op='...,...'):
     
     axes specify the axes over which convolutions occur
     
-    circular_list specifies whether to do a circular or a regular convolution
+    circular_list (True or False) specifies whether to do a circular or a regular convolution
     
-    op specifies the tensor operation which to perform to the ffts
+    op specifies the tensor operation which to perform to the intermediate ffts
+    it is a string which becomes the first argument to numpy.einsum
+    by default it is elementwise multiplication
+    '''
+    
+    if izero is None:
+        izero = len(a)//2
+    
+    a_ = a[:]
+    b_ = b[:]
+    
+    dims = array([min(d1,d2) for d1,d2 in zip(shape(a), shape(b))])
+    #maxdims = array([max(d1,d2) for d1,d2 in zip(shape(a), shape(b))])
+    
+    for indices, axis, circular in zip(indices_list, axes, circular_list):
+        if circular: continue
+            
+        comma = indices.index(',')
+        
+        a_ = concatenate((a_, zeros(2*dims-array(shape(a)))), axis=axis)
+        b_ = concatenate((b_, zeros(2*dims-array(shape(b)))), axis=axis) 
+    
+    for axis in axes:
+        a_ = roll(a_, -izero, axis=axis)
+        b_ = roll(b_, -izero, axis=axis)
+        
+    a_ = fft.fftn(a_, axes=axes)
+    b_ = fft.fftn(b_, axes=axes)
+        
+    for indices, axis in zip(indices_list, axes):
+        if '+' in indices:
+            comma = indices.index(',')
+            if comma==1:
+                a_ = roll(flip(a_, axis=axis), 1, axis=axis)
+            elif comma==3:
+                b_ = roll(flip(b_, axis=axis), 1, axis=axis)
+            else: raise ValueError
+            
+    x = fft.ifftn(einsum(op, a_, b_), axes=axes)
+    for axis in axes:
+        x = roll(x, izero, axis=axis)
+    
+    return x
+
+
+
+def basic_conv0(a, b, indices_list, axes, circular_list, op='...,...'):
+    '''
+    todo: write optional zeros argument
+      - if x is the repeated index, then yz-xz=0 and xz=0 and yz=0
+    
+    a and b must be the same size
+    the length of each axis must be even
+    
+    indices_list specifies the way in which to convovle for the corresponding axes in 'axes'
+    the repeated index is summed over
+    the single index is the remaining index after convolution
+    for example, indices = ['x,y-x'] will perform the sum over x of a(x)*b(y-x)
+    
+    options for each entry of indices_list is one of four forms (you can change the letter names):
+    'x,y-x' or 'x,x+y' or 'y-x,x'  or 'x+y,x' 
+    
+    every convolution can be put into one of these forms by relabeling the index which is summed over
+    note that 'x,x-y' is not in one of these forms but after the relabeling x->x+y it becomes 'x+y,x'
+    
+    axes specify the axes over which convolutions occur
+    
+    circular_list (True or False) specifies whether to do a circular or a regular convolution
+    
+    op specifies the tensor operation which to perform to the intermediate ffts
     it is a string which becomes the first argument to numpy.einsum
     by default it is elementwise multiplication
     '''
---
+++ imagaxis/test/test_conv.py
@@ -0,0 +1,100 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jul 30 09:29:32 2020
+
+@author: benno
+"""
+
+import numpy as np
+from convolution import basic_conv
+
+
+n = 20
+A = np.random.randn(n)
+B = np.random.randn(n)
+
+# convolve
+C0 = basic_conv(A, B, ['x,y-x'], [0], [False])[:n]
+#print(C0.real)
+
+# explicit convolve (assuming index 0 is at n//2)
+C1 = np.zeros(n)
+
+for iy in range(n):
+    for ix in range(n):
+        # when iy = n//2
+        # and  ix = n//2   
+        # C[iy] = A[ix] B[iy - ix + n//2]
+    
+        if iy - ix + n//2 >=0 and iy - ix + n//2 < n:   
+            C1[iy] += A[ix] * B[iy - ix + n//2]
+
+print(np.mean((C0-C1)).real)
+
+
+
+
+# convolve
+C0 = basic_conv(A, B, ['x,x+y'], [0], [False])[:n]
+#print(C0.real)
+
+# explicit convolve (assuming index 0 is at n//2)
+C1 = np.zeros(n)
+
+for iy in range(n):
+    for ix in range(n):
+        # when iy = n//2
+        # and  ix = n//2  
+        # C[iy] = A[ix] B[ix + iy - n//2]
+    
+        if ix + iy - n//2 >=0 and ix + iy - n//2 < n:
+            C1[iy] += A[ix] * B[ix + iy - n//2]
+
+print(np.mean((C0-C1)).real)
+
+
+
+
+
+# convolve
+C0 = basic_conv(A, B, ['x,y-x'], [0], [False], izero=4)[:n]
+#print(C0.real)
+
+
+# explicit convolve (where index 0 is at iz)
+C1 = np.zeros(n)
+
+iz = 4
+for iy in range(n):
+    for ix in range(n):
+        # when iy = iz
+        # and  ix = iz   
+        # C[iy] = A[ix] B[iy - ix + iz]
+    
+        if iy - ix + iz >=0 and iy - ix + iz < n:   
+            C1[iy] += A[ix] * B[iy - ix + iz]
+
+print(np.mean((C0-C1)).real)
+
+
+# convolve
+C0 = basic_conv(A, B, ['x,x+y'], [0], [False], izero=4)[:n]
+#print(C0.real)
+
+
+# explicit convolve (where index 0 is at iz)
+C1 = np.zeros(n)
+
+iz = 4
+for iy in range(n):
+    for ix in range(n):
+        # when iy = n//2
+        # and  ix = n//2  
+        # C[iy] = A[ix] B[ix + iy - n//2]
+    
+        if ix + iy - iz >=0 and ix + iy - iz < n:
+            C1[iy] += A[ix] * B[ix + iy - iz]
+
+print(np.mean((C0-C1)).real)
+
+
